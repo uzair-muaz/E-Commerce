871ed74cecd26015848f7fd2bb416f79370f194b
Uzair <uzair.muaz3k@gmail.com>
2022-12-14 02:28:31 +0500

    Product List Added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from '../Components/Layout';
+import ProductItem from '../Components/ProductItem';
+import data from '../utils/data';
 
 export default function Home() {
-  return <Layout title={'Home Page'}>Home Page</Layout>;
+  return (
+    <Layout title={'Home Page'}>
+      <section className="grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4 ">
+        {data.products.map((product) => (
+          <ProductItem key={product.slag} product={product} />
+        ))}
+      </section>
+    </Layout>
+  );
 }
